ee727897fd10d5e67620e9829925657a4af448b5
bcadolf <adolfbrandon96@gmail.com>
2025-07-01 13:10:06 -0400

    chapt 10 done

--- learn-next-app-router/nextjs-dashboard/next.config.ts
+++ learn-next-app-router/nextjs-dashboard/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // experimental: {
+  //   ppr: "incremental",
+  // }, used to test ppr
 };
 
 export default nextConfig;
